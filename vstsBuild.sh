362fd4b7bea829b266793e7ecb7a945450e7f614
Steve White <steve.white@smoothwall.com>
2019-02-13 09:24:51 +0000

    Change EOLs

--- vstsBuild.sh
+++ vstsBuild.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Build"
 ./build.sh
 [ "$?" != "0" ] && echo "ERROR in conan_build" && exit 1
 
+echo "Conan export"
 ./conanExport.sh
 [ "$?" != "0" ] && echo "ERROR in conan_export" && exit 2
